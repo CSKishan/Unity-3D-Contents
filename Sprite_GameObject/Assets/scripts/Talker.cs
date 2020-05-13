d6d98bc940ae787a415f73b5704ad2ad7f1e012d
Kishan <kishanland@gmail.com>
2020-05-13 18:43:08 +0530

    Physics in Unity
    Fly birdy! Fly!

---
+++ Sprite_GameObject/Assets/scripts/Talker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talker : MonoBehaviour
+{
+    // Makes the game element talk
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Vector3 position = transform.position;
+        position.x = 1;
+        //Say something 
+        print("Im existing for a reason.");
+    }
+
+}
